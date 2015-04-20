9a22922c688c6f0cfd2187d1d52cf89b466c20a6
dewinurbuaty <fb.depe@gmail.com>
2015-04-20 13:20:13 +0700

    Create Alchymia.java

---
+++ Alchymia.java
@@ -0,0 +1,10 @@
+//kelas Alchymia
+//merupakan turunan dari kelas Monster
+//dewinurbuaty 18213039
+
+public class Alchymia extends Monster /*implements attackedby*/ {
+	public Alchymia(){
+		//nama, atk, hp, money, incLevel
+		super("Alchymia",5,200,100,1);
+	}	
+}
